6ee544ede5974e24ae19411f9cc4b151b662ba4f
DianaQuispe <danaalienmas@gmail.com>
2017-08-22 18:13:00 -0500

    cmit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+var imgArray= Array.from(document.getElementsByClassName("imagenes"));
+var product=document.getElementById("product");
+imgArray.forEach(function(img) {
+    img.addEventListener("click",agregar);
+});
+
+function agregar() {
+    var product=document.getElementById("product");
+
+    var divsote = document.createElement("div");
+    divsote.setAttribute("id","divG");
+    var divsito = document.createElement("div");
+    divsito.setAttribute("class","divC");
+    var image = document.createElement("img");
+    divsote.appendChild(divsito);
+    product.appendChild(divsote);
+     
+}
