a7b88fdf15954796cd20ca03e5b3a83a48e3acb3
MyDiana <danaalienmas@gmail.com>
2017-08-23 07:24:18 -0500

    c

--- js/app.js
+++ js/app.js
@@ -1,32 +1,35 @@
+
 var imgArray= Array.from(document.getElementsByClassName("imagenes"));
-for(var i=1; i<=imgArray.length; i++) {
-    var index = imgArray.indexOf("../assets/images/img"+2+".jpg");
-    }
+  var images = ["../assets/images/img-1.jpg", "../assets/images/img-2.jpg", "../assets/images/img-3.jpg", "../assets/images/img-4.jpg", "../assets/images/img-5.jpg", "../assets/images/img-6.jpg", "../assets/images/img-7.jpg", "../assets/images/img-8.jpg", "../assets/images/img-9.jpg"];
 var product=document.getElementById("product");
 imgArray.forEach(function(img) {
-   
-
     img.addEventListener("click",agregar);
 
 });
+    var images = ["../assets/images/img-1.jpg", "../assets/images/img-2.jpg", "../assets/images/img-3.jpg", "../assets/images/img-4.jpg", "../assets/images/img-5.jpg", "../assets/images/img-6.jpg", "../assets/images/img-7.jpg", "../assets/images/img-8.jpg", "../assets/images/img-9.jpg"];
 
-function agregar() {
-     for(var i=1; i<=9; i++) {
-        var index = imgArray.indexOf(i);
+     for(var i=0; i<images.length; i++) {
+        var index = images[i];
+   var image = document.createElement("img");
+         image.setAttribute(index);
     }
+function agregar() {
+    var index;
+
+
      product=document.getElementById("product");
     var divsote = document.createElement("div");
     divsote.setAttribute("id","divG");
     var divsito = document.createElement("div");
     divsito.setAttribute("class","divC");
-    var image = document.createElement("img");
+
     var nuevospan =document.createElement("span");
     nuevospan.setAttribute("class","span");
     var textospan= document.createTextNode("X");
     nuevospan.addEventListener("click",borrar);
     nuevospan.appendChild(textospan);
-    
-    image.setAttribute("src",index);
+
+    image.setAttribute("src","../assets/images/img-"+i+".jpg");
     image.setAttribute("class","image");
     divsito.appendChild(image);
     divsito.appendChild(nuevospan);
@@ -36,7 +39,7 @@ function agregar() {
 function borrar() {
     var nuevospan = document.getElementsByClassName("span");
     var divsote = document.getElementById('divG');
-    var product=document.getElementById("product");        
+    var product=document.getElementById("product");
     product.removeChild(divsote);
-    
+
 }
