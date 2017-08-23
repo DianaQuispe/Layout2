8f1c07240a61c5ba6427780de707474cba9b51db
DianaQuispe <danaalienma@gmail.com>
2017-08-22 22:54:55 -0500

    sct

--- js/app.js
+++ js/app.js
@@ -2,17 +2,39 @@ var imgArray= Array.from(document.getElementsByClassName("imagenes"));
 var product=document.getElementById("product");
 imgArray.forEach(function(img) {
     img.addEventListener("click",agregar);
+
 });
+for(var i=1; i<9; i++) {
+        console.log(i);
+    }
 
 function agregar() {
-    var product=document.getElementById("product");
-
+    
+     product=document.getElementById("product");
     var divsote = document.createElement("div");
     divsote.setAttribute("id","divG");
     var divsito = document.createElement("div");
     divsito.setAttribute("class","divC");
     var image = document.createElement("img");
+    var nuevospan =document.createElement("span");
+    nuevospan.setAttribute("class","span");
+    var textospan= document.createTextNode("X");
+    nuevospan.addEventListener("click",borrar);
+    nuevospan.appendChild(textospan);
+    
+          image.setAttribute("src","../assets/images/img-"+i+".jpg")
+
+
+    image.setAttribute("class","image");
+    divsito.appendChild(image);
+    divsito.appendChild(nuevospan);
     divsote.appendChild(divsito);
     product.appendChild(divsote);
-     
+}
+function borrar() {
+    var nuevospan = document.getElementsByClassName("span");
+    var divsote = document.getElementById('divG');
+    var product=document.getElementById("product");        
+    product.removeChild(divsote);
+    
 }
